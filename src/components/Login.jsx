9667451d0966773cb0e192c72142fa0dc4a2bba6
Abhishek Panwar <imabhishek612@gmail.com>
2023-06-05 01:01:12 +0530

    implemented auth, fixing builder error

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import GoogleLogin from "react-google-login";
-
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
+import client from "../client";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    localStorage.setItem("user", JSON.stringify(response.profileObj));
+    const { name, googleId, imageUrl } = response.profileObj;
+
+    const doc = {
+      _id: googleId,
+      _type: "user",
+      userName: name,
+      image: imageUrl,
+    };
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className="ralative w-full h-full">
@@ -24,8 +41,21 @@ const Login = () => {
         </div>
         <div className="shadow-2xl">
           <GoogleLogin
-            clientId=""
-            render={(renderProps) => <button></button>}
+            clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+            render={(renderProps) => (
+              <button
+                type="button"
+                className="bg-mainColor  flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                <FcGoogle className="mr-4" />
+                Sign in with Google
+              </button>
+            )}
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy="single_host_origin"
           />
         </div>
       </div>
